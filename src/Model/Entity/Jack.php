731d75c3e2afa4b4a7a1ba26214e186f8c0a32cb
muqeetsyed <muqeet@yarikul.in>
2022-12-01 08:44:42 +0000

    added setup for phpunit

---
+++ src/Model/Entity/Jack.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Jack
+{
+
+    public string $one = '';
+    public string $two = '';
+
+    public function __construct(string $one, string $two)
+    {
+
+    }
+
+}
---
+++ tests/Model/Entity/JackTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Model\Entity;
+
+
+
+use PHPUnit\Framework\TestCase;
+
+class JackTest extends TestCase
+{
+   public function test_one(){
+      self::assertEquals(2,2);
+   }
+}
